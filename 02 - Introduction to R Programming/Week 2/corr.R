497f584e2a5b95db529948793279cabaed217d75
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2015-05-18 13:45:04 -0400

    Add comments to corr.R

--- 02 - Introduction to R Programming/Week 2/corr.R
+++ 02 - Introduction to R Programming/Week 2/corr.R
@@ -1,15 +1,19 @@
 corr <- function(directory, threshold=0) {
   
-  ##Initialize df of complete cases & numeric vector to store correlations
+  ##Initialize numeric vector to store correlations & df of complete cases over threshold
   datacor <- numeric()
   com <- complete(directory)
   com <- com[com$nobs > threshold,]
   
+  ##Loop over files in directory that contain complete cases over the threshold and add to datacor vector
   for(i in com$id) {
     file <- read.csv(file.path(directory, paste(formatC(i, width=3, flag=0), ".csv", sep = "")))
     datacor[i] <- cor(file[["sulfate"]], file[["nitrate"]], use ="complete.obs")
   }
   
+  ##Remove NA values from datacor vector
   datacor <- datacor[!is.na(datacor)]
+  
+  ##Return datacor vector
   return(datacor)
 }
